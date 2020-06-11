ef61703c187f2e011f6b550d9df522d2ac9bc795
laozhou <imanage@aliyun.com>
2020-06-11 14:13:54 +0800

    UI版本

---
+++ moosnow.conf.js
@@ -0,0 +1,52 @@
+window.moosnowConfig = {
+    debug:'wx',
+    /**
+     * 微信平台
+     */
+    wx: {
+        bannerId: "adunit-e51b3123060eec9e",   //请填写你自己的APP banner id
+        videoId: "adunit-a322f5ee40076372",    //请填写你自己的APP video id
+        interId: "adunit-7c61767905a3940a", //请填写你自己的APP inter id   
+        nativeId: "",
+        moosnowAppId: "wxeea828178bcda2e9",
+        version: "1.1.0",
+        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_hzyx_config_wx.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+    },
+    oppo: {
+        bannerId: "168776",
+        videoId: "168781",
+        interId: "168777",
+        nativeId: ["168779", "168780"],
+        moosnowAppId: "30251588",
+        version: "1.1.0",
+        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_hzyx_config_wx.json",
+    },
+    qq: {
+        bannerId: "393fd2c197f4fdda1f9729ef36737890",
+        videoId: "009f6251b101f9eeb54fb202d537e6d6",
+        interId: "",
+        boxId: "32b9a79d89010c17850ba54da9dab60f", //qq游戏特有的广告
+        nativeId: ["", ""],
+        moosnowAppId: "1110381022",
+        version: "1.1.0",
+        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+    },
+    bd: {
+        bannerId: "168776",
+        videoId: "168781",
+        interId: "168777",
+        nativeId: ["168779", "168780"],
+        moosnowAppId: "30251588",
+        version: "1.1.0",
+        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+    },
+    byte: {
+        bannerId: "168776",
+        videoId: "168781",
+        interId: "168777",
+        nativeId: ["168779", "168780"],
+        moosnowAppId: "30251588",
+        version: "1.1.0",
+        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+    }
+}
