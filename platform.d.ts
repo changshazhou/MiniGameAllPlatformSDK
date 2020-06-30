22700f1dc8922fcd578ea1910992733430f3302c
laozhou <imanage@aliyun.com>
2020-06-30 15:27:56 +0800

    修复VIVO下可能导致请求数据得BUG，增加分享录屏时间短暂得回调

--- platform.d.ts
+++ platform.d.ts
@@ -365,12 +365,13 @@ declare class PlatformModule extends BaseModule {
     _bottomCenterBanner(size: any): void;
     _resetBanenrStyle(size: any): void;
     /**
-     *
+     * 显示平台的banner广告
+     * @param remoteOn 是否被后台开关控制 默认 true，误触的地方传 true  普通的地方传 false
      * @param callback 点击回调
      * @param position banner的位置，默认底部
      * @param style 自定义样式
      */
-    showBanner(callback?: Function, position?: string, style?: bannerStyle): void;
+    showBanner(remoteOn?: boolean, callback?: (isOpend: boolean) => void, position?: string, style?: bannerStyle): void;
     /**
      * 会自动隐藏的banner
      * 一般用游戏中
