794ece6204a8598bb037e837c529bf9aa78762f7
laozhou <imanage@aliyun.com>
2020-07-15 08:58:13 +0800

    修改提示文件

---
+++ demo.ts
@@ -0,0 +1,5 @@
+export default class demo {
+    test() {
+
+    }
+}
--- platform.d.ts
+++ platform.d.ts
@@ -598,75 +598,6 @@ declare class EventType {
     static COIN_CHANGED: string;
     static RANDOWM_NAVIGATE: string;
 }
-declare const AD_POSITION = {
-    /**
-     * 不显示
-     */
-    NONE: 0,
-    BANNER: 1,
-    FLOAT: 2,
-    /**
-     * 侧拉广告
-     */
-    SIDE: 4,
-    /**
-     * 中部大导出
-     */
-    CENTER: 8,
-    /**
-     * 导出
-     */
-    EXPORT: 16,
-
-    /**
-     * 返回按钮
-     */
-    BACK: 32,
-    /**
-     * 黑色半透明遮挡
-     */
-    MASK: 64,
-    /**
-     * 延迟显示
-     */
-    WAIT: 128,
-
-    /**
-     * 左右两侧
-     */
-    LEFTRIGHT: 256,
-
-    /**
-    * 固定的六个
-    */
-    EXPORT_FIXED: 512,
-
-    /**
-    * 扩展1
-    */
-    EXTEND1: 1024,
-    /**
-    * 扩展2
-    */
-    EXTEND2: 2048,
-    /**
-    * 扩展3
-    */
-    EXTEND3: 4096,
-    /**
-    * 扩展4
-    */
-    EXTEND4: 8192,
-
-    /**
-     * 顶部
-     */
-    TOP: 32768,
-    /**
-     * 恢复到上一个状态
-     */
-    RECOVER: 16384,
-}
 declare class moosnow {
 
 
@@ -694,9 +625,77 @@ declare class moosnow {
         TOKEN: string;
         LINK: string;
     };
-    static AD_POSITION = AD_POSITION;
-    static PLATFORM_EVENT = typeof EventType;
-    static APP_PLATFORM = typeof PlatformType;
+    static AD_POSITION: {
+        /**
+         * 不显示
+         */
+        NONE: 0,
+        BANNER: 1,
+        FLOAT: 2,
+        /**
+         * 侧拉广告
+         */
+        SIDE: 4,
+        /**
+         * 中部大导出
+         */
+        CENTER: 8,
+        /**
+         * 导出
+         */
+        EXPORT: 16,
+
+        /**
+         * 返回按钮
+         */
+        BACK: 32,
+        /**
+         * 黑色半透明遮挡
+         */
+        MASK: 64,
+        /**
+         * 延迟显示
+         */
+        WAIT: 128,
+
+        /**
+         * 左右两侧
+         */
+        LEFTRIGHT: 256,
+
+        /**
+        * 固定的六个
+        */
+        EXPORT_FIXED: 512,
+
+        /**
+        * 扩展1
+        */
+        EXTEND1: 1024,
+        /**
+        * 扩展2
+        */
+        EXTEND2: 2048,
+        /**
+        * 扩展3
+        */
+        EXTEND3: 4096,
+        /**
+        * 扩展4
+        */
+        EXTEND4: 8192,
+
+        /**
+         * 顶部
+         */
+        TOP: 32768,
+        /**
+         * 恢复到上一个状态
+         */
+        RECOVER: 16384,
+    };
+    static PLATFORM_EVENT = EventType;
+    static APP_PLATFORM = PlatformType;
     static getAppPlatform(): PlatformType
     static http: HttpModule
     static platform: PlatformModule
