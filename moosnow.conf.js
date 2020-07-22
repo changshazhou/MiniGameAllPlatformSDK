cdeb5937053d5b85e787ecb24c8c356a6bd2f900
laozhou <imanage@aliyun.com>
2020-07-22 16:20:03 +0800

    更新配置

--- moosnow.conf.js
+++ moosnow.conf.js
@@ -1,5 +1,5 @@
 window.moosnowConfig = {
-    debug:'wx',
+    debug: 'wx',
     /**
      * 微信平台
      */
@@ -9,8 +9,7 @@ window.moosnowConfig = {
         interId: "adunit-7c61767905a3940a", //请填写你自己的APP inter id   
         nativeId: "",
         moosnowAppId: "wxeea828178bcda2e9",
-        version: "1.1.0",
-        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_hzyx_config_wx.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+        version: "1.1.0"
     },
     oppo: {
         bannerId: "168776",
@@ -18,8 +17,15 @@ window.moosnowConfig = {
         interId: "168777",
         nativeId: ["168779", "168780"],
         moosnowAppId: "30251588",
-        version: "1.1.0",
-        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_hzyx_config_wx.json",
+        version: "1.1.0"
+    },
+    vivo: {
+        bannerId: "aa9a8f1e5403435fa51ec680dbbf0da2",
+        videoId: "c8bf3259afd64e389e42f1b67e21e005",
+        interId: "168777",
+        nativeId: ["7d595c09da994cc58c02217f43ebdd40", "010b436a80974dd6b3eafffc6c94b974"],
+        moosnowAppId: "100006157",
+        version: "1.1.0"
     },
     qq: {
         bannerId: "393fd2c197f4fdda1f9729ef36737890",
@@ -28,8 +34,7 @@ window.moosnowConfig = {
         boxId: "32b9a79d89010c17850ba54da9dab60f", //qq游戏特有的广告
         nativeId: ["", ""],
         moosnowAppId: "1110381022",
-        version: "1.1.0",
-        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+        version: "1.1.0"   url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
     },
     bd: {
         bannerId: "168776",
@@ -37,8 +42,7 @@ window.moosnowConfig = {
         interId: "168777",
         nativeId: ["168779", "168780"],
         moosnowAppId: "30251588",
-        version: "1.1.0",
-        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+        version: "1.1.0"
     },
     byte: {
         bannerId: "168776",
@@ -46,7 +50,6 @@ window.moosnowConfig = {
         interId: "168777",
         nativeId: ["168779", "168780"],
         moosnowAppId: "30251588",
-        version: "1.1.0",
-        url: "https://liteplay-1253992229.cos.ap-guangzhou.myqcloud.com/config/tp_zzxhcar_config.json",//游戏的配置json，如果用到了getMistouchNum,后台必须配置mistouchNum字段
+        version: "1.1.0"
     }
 }
